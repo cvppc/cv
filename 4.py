ed83466f78a937a388c4f70544218f9474d1d9c4
cvppc <153167336+cvppc@users.noreply.github.com>
2023-12-07 13:54:42 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,50 +1,116 @@
-import numpy as np
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-def get_background(file_path):
-    cap = cv2.VideoCapture(file_path)
-    frame_indices = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=50)
-    frames = []
-    for idx in frame_indices:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
-        ret, frame = cap.read()
-        frames.append(frame)
-    median_frame = np.median(frames, axis=0).astype(np.uint8)
-    return median_frame 
+def create_output(vertices, colors, filename):
+    colors = colors.reshape(-1, 3)
+    vertices = np.hstack([vertices.reshape(-1, 3), colors])
+    ply_header = '''ply
+    format ascii 1.0
+    element vertex %(vert_num)d
+    property float x
+    property float y
+    property float z
+    property uchar red
+    property uchar green
+    property uchar blue
+    end_header
+    '''
+    with open(filename, 'w') as f:
+        f.write(ply_header % dict(vert_num=len(vertices)))
+        np.savetxt(f, vertices, '%f %f %f %d %d %d')
 
-cap = cv2.VideoCapture('input.mp4')
-background = get_background('nput.mp4')
-background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
-frame_count = 0
-consecutive_frame = 10
+def downsample_image(image, reduce_factor):
+    for _ in range(reduce_factor):
+        if len(image.shape) > 2:
+            row, col = image.shape[:2]
+        else:
+            row, col = image.shape
+        image = cv2.pyrDown(image, dstsize=(col // 2, row // 2))
+    return image
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        frame_count += 1
-        orig_frame = frame.copy()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if frame_count % consecutive_frame == 0 or frame_count == 1:
-            frame_diff_list = []
-        frame_diff = cv2.absdiff(gray, background)
-        ret, thres = cv2.threshold(frame_diff, 50, 255, cv2.THRESH_BINARY)
-        dilate_frame = cv2.dilate(thres, None, iterations=2)
-        frame_diff_list.append(dilate_frame)
-        if len(frame_diff_list) == consecutive_frame:
-            sum_frames = sum(frame_diff_list)
-            contours, hierarchy = cv2.findContours(sum_frames, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            for i, cnt in enumerate(contours):
-                cv2.drawContours(frame, contours, i, (0, 0, 255), 3)
-            for contour in contours:
-                if cv2.contourArea(contour) < 500:
-                    continue
-                (x, y, w, h) = cv2.boundingRect(contour)
-                cv2.rectangle(orig_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.imshow("hi",orig_frame)
-            if cv2.waitKey(100) & 0xFF == ord('q'):
-                break
-    else:
-        break
+# Load calibration data
+# ret = np.load('/content/ret.npy')
+# K = np.load('/content/K.npy')
+# dist = np.load('/content/dist.npy')
 
-cap.release()
-cv2.destroyAllWindows()
+ret = None
+K = np.eye(3)  # Identity matrix as a placeholder
+dist = np.zeros((5, 1))  # Zeros as a placeholder
+
+# Load images
+# NANBARGALEEEE NOTE THE BELOW
+# note that load images of same size. If the size are not equal resize it
+
+img_path1 = "C:/Users/Arwin/Pictures/profile1.jpg"
+img_path2 = "a.jpg"
+img_1 = cv2.imread(img_path1)
+img_2 = cv2.imread(img_path2)
+
+# Undistort images
+h, w = img_2.shape[:2]
+new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(K, dist, (w, h), 1, (w, h))
+img_1_undistorted = cv2.undistort(img_1, K, dist, None, new_camera_matrix)
+img_2_undistorted = cv2.undistort(img_2, K, dist, None, new_camera_matrix)
+
+# Downsample images
+img_1_downsampled = downsample_image(img_1_undistorted, 3)
+img_2_downsampled = downsample_image(img_2_undistorted, 3)
+
+"""use the below to resize ther image. dont change or use new variable instead update the variable. UNCOMMENT THE BELOW TO RESIZE"""
+img_1_downsampled  = cv2.resize(img_1_downsampled, (960, 540)) 
+img_2_downsampled = cv2.resize(img_2_downsampled, (960, 540))
+
+"""THESE LINES ARE USED TO CHECK THE SIZE AND TYPE OF THE IMG"""
+print(img_1_downsampled.shape, img_1_downsampled.dtype)
+print(img_2_downsampled.shape, img_2_downsampled.dtype)
+
+
+# Stereo matching parameters
+win_size = 5
+min_disp = -1
+max_disp = 63
+num_disp = max_disp - min_disp
+stereo = cv2.StereoSGBM_create(
+    minDisparity=min_disp,
+    numDisparities=num_disp,
+    blockSize=5,
+    uniquenessRatio=5,
+    speckleWindowSize=5,
+    speckleRange=5,
+    disp12MaxDiff=2,
+    P1=8 * 3 * win_size**2,
+    P2=32 * 3 * win_size**2
+)
+
+# Compute disparity map
+print("\nComputing the disparity map...")
+disparity_map = stereo.compute(img_1_downsampled, img_2_downsampled)
+plt.imshow(disparity_map, 'gray')
+plt.show()
+
+# Generate 3D map
+print("\nGenerating the 3D map...")
+# focal_length = np.load('/content/FocalLength.npy')
+# Use a default or estimated focal length (example value)
+focal_length = 1000.0  
+Q = np.float32([[1, 0, 0, -w / 2.0],
+                [0, -1, 0, h / 2.0],
+                [0, 0, 0, -focal_length],
+                [0, 0, 1, 0]])
+Q2 = np.float32([[1, 0, 0, 0],
+                 [0, -1, 0, 0],
+                 [0, 0, focal_length * 0.05, 0],
+                 [0, 0, 0, 1]])
+points_3D = cv2.reprojectImageTo3D(disparity_map, Q2)
+
+# Get colors for 3D points
+colors = cv2.cvtColor(img_1_downsampled, cv2.COLOR_BGR2RGB)
+mask_map = disparity_map > disparity_map.min()
+output_points = points_3D[mask_map]
+output_colors = colors[mask_map]
+
+# Output file
+output_file = 'reconstructed.ply'
+print("\nCreating the output file...\n")
+create_output(output_points, output_colors, output_file)
